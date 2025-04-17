707949517d0600db5a97ed39bb32fd470bfc4b7a
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-04-17 02:46:07 +0000

    Set up main App component with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import { useState } from 'react'
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
+import MainLayout from './components/layout/MainLayout';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import ForgotPasswordPage from './pages/auth/ForgotPasswordPage';
+import DashboardPage from './pages/dashboard/DashboardPage';
+import ContactsPage from './pages/contacts/ContactsPage';
+import ContactDetailPage from './pages/contacts/ContactDetailPage';
+import ContactFormPage from './pages/contacts/ContactFormPage';
+import TagsPage from './pages/tags/TagsPage';
+import SettingsPage from './pages/settings/SettingsPage';
+import { Toaster } from 'sonner';
 
 function App() {
   return (
-    <div></div>
-  )
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Auth Routes */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+          
+          {/* Protected Routes */}
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<DashboardPage />} />
+            <Route path="contacts" element={<ContactsPage />} />
+            <Route path="contacts/:id" element={<ContactDetailPage />} />
+            <Route path="contacts/new" element={<ContactFormPage />} />
+            <Route path="contacts/edit/:id" element={<ContactFormPage />} />
+            <Route path="tags" element={<TagsPage />} />
+            <Route path="settings" element={<SettingsPage />} />
+          </Route>
+          
+          {/* Fallback Route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+      <Toaster position="top-right" />
+    </AuthProvider>
+  );
 }
 
-export default App 
+export default App;
